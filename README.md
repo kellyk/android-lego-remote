2b490c292136684433002e8f711649e86fe9cf14
Kelly King <kelly.danielle.king@gmail.com>
2013-04-02 17:25:57 -0400

    updates README

---
+++ README.md
@@ -0,0 +1,3 @@
+Native Android app which uses bluetooth to control LEGO Mindstorms NXT robots.
+
+!In early development!
--- src/com/example/robotv1/MainActivity.java
+++ src/com/example/robotv1/MainActivity.java
@@ -41,8 +41,15 @@ public class MainActivity extends Activity {
 				
 				@Override
 				public void onClick(View v) {
-					TextView tv = (TextView) findViewById(R.id.ui_connect_status);
-					tv.setText("Connect button pressed");
+					Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+					// If there are paired devices
+					if (pairedDevices.size() > 0) {
+					    // Loop through paired devices
+					    for (BluetoothDevice device : pairedDevices) {
+					        // Add the name and address to an array adapter to show in a ListView
+					        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+					    }
+					}
 				}
 
 			});
